106a842957a63980c46aa8a05dbb73d8ffa10048
chuxin <15115359055@139.com>
2024-05-22 11:19:23 +0800

    feat: 新增layout布局组件

---
+++ src/layouts/index.ts
@@ -0,0 +1,6 @@
+import Breadcrumb from "./src/Breadcrumb/index.vue";
+import Header from "./src/Header";
+import Main from "./src/Main";
+import Menu from "./src/Menu";
+
+export { Breadcrumb, Header, Main, Menu };
---
+++ src/layouts/src/Breadcrumb/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Breadcrumb</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Breadcrumb",
+};
+</script>
