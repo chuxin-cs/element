41211f3f2e31896001e058b90909a195a7d7974c
chuxin <15115359055@139.com>
2024-05-23 10:31:04 +0800

    feat: layout 布局

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,6 +1,6 @@
 import Breadcrumb from "./src/Breadcrumb/index.vue";
-import Header from "./src/Header";
-import Main from "./src/Main";
-import Menu from "./src/Menu";
+import Header from "./src/Header/index.vue";
+import Main from "./src/Main/index.vue";
+import Menu from "./src/Menu/index.vue";
 
 export { Breadcrumb, Header, Main, Menu };
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,10 @@ import type { RouteRecordRaw } from "vue-router";
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import { basicRoutes } from "./pages";
+import { asyncRoutes } from "./views";
 
 // 现在第一步先把权限都整到这里
-const routes: RouteRecordRaw[] = [...basicRoutes];
+const routes: RouteRecordRaw[] = [...basicRoutes, ...asyncRoutes];
 
 export const router = createRouter({
   history: createWebHashHistory(),
