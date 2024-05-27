ed09ce5756bc53f58cc0fb5dbdeeaa43315684f6
chuxin <15115359055@139.com>
2024-05-27 09:27:46 +0800

    feat: 修改 vue.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import * as path from "path";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
@@ -16,6 +17,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    VueDevTools(),
   ],
   resolve: {
     alias: {
