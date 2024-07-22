a6d05c03676f715292a022fc65697e351698002e
chuxin <15115359055@139.com>
2024-07-22 10:28:06 +0800

    feat: 用于 第03期：Vite 插件从入门到放弃~ 代码演示

--- demo-plugins/vite.config.ts
+++ demo-plugins/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import Inspector from 'vite-plugin-vue-inspector'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Inspector from "vite-plugin-vue-inspector";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),Inspector(),vueDevTools()],
-})
+  plugins: [vue(), Inspector(), vueDevTools()],
+});
